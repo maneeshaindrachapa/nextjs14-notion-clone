a1c0757db5b0eea7a29c3dd8fcfe94c87089f52b
maneeshaindrachapa <maneeshaindrachapa@gmail.com>
2024-04-30 10:57:18 +0200

    adding necessary packages and configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+      },
+      {
+        protocol: "http",
+        hostname: "files.edgestore.dev",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
