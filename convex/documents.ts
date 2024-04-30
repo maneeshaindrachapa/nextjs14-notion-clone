fb3c85ce83e5f440551ded661043193fd6279bcc
maneeshaindrachapa <maneeshaindrachapa@gmail.com>
2024-04-30 10:57:29 +0200

    updating convex schema

--- convex/documents.ts
+++ convex/documents.ts
@@ -233,27 +233,21 @@ export const getById = query({
   args: { documentId: v.id("documents") },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-
     const document = await ctx.db.get(args.documentId);
-
     if (!document) {
       throw new Error("Document not found");
     }
-
     if (document.isPublished && !document.isArchived) {
       return document;
     }
-
     if (!identity) {
       throw new Error("Not authenticated");
     }
 
     const userId = identity.subject;
-
     if (document.userId !== userId) {
       throw new Error("Not authorized");
     }
-
     return document;
   },
 });
